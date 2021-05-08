e70912a8c52295b1455499a1987b9d83efec745d
Axel Ulmestig <axel.ulmestig@gmail.com>
2021-05-08 10:23:25 +0200

    add SECRET env var

--- src/Config.hs
+++ src/Config.hs
@@ -11,7 +11,7 @@ import           Control.Lens
 import           Control.Monad.Except
 import           Control.Monad.Trans.Except
 import           Data.Aeson
-import qualified Data.ByteString.Lazy       as BS
+import qualified Data.ByteString.Lazy       as LBS
 import qualified Data.ByteString.Lazy.Char8 as BSC8
 import qualified Data.ByteString.UTF8       as BSU
 import           Data.Char                  (toLower)
@@ -22,6 +22,7 @@ import           System.Environment         (lookupEnv)
 data Config = Config {
   requestOptions :: Network.Wreq.Options,
   jiraUrl        :: String,
+  secret         :: BSU.ByteString,
   desiredColumn  :: String -> String -> Maybe String
 }
 
@@ -44,8 +45,8 @@ getJiraCredentials = do
 
 getBranchColumnMapping :: ExceptT ConfigError IO (String -> String -> Maybe String)
 getBranchColumnMapping = do
-  -- TODO: catch errors from `BS.readFile`. `catchE` from Control.Monad.Trans.Except ?
-  fileContents <- lift $ BS.readFile "./branch-column-mapping.json"
+  -- TODO: catch errors from `LBS.readFile`. `catchE` from Control.Monad.Trans.Except ?
+  fileContents <- lift $ LBS.readFile "./branch-column-mapping.json"
   case decode fileContents of
     Nothing   -> except $ Left $ BranchColumnMappingParseError (BSC8.unpack fileContents)
     Just dict -> return $ \branch proj -> M.lookup (fmap toLower branch) dict >>= M.lookup (fmap toLower proj)
@@ -54,11 +55,13 @@ getConfig :: ExceptT ConfigError IO Config
 getConfig = do
   jiraUrl <- getEnvVariable "JIRA_URL"
   (jiraUsername, jiraPassword) <- getJiraCredentials
+  secret <- getEnvVariable "SECRET"
   let opts = set auth (Just (basicAuth jiraUsername jiraPassword)) defaults
   branchColumnMapping <- getBranchColumnMapping
 
   return Config {
-      jiraUrl = jiraUrl,
-      requestOptions = set (header "Content-Type") (["application/json"]) opts,
-      desiredColumn = branchColumnMapping
+      jiraUrl         = jiraUrl,
+      secret          = BSU.fromString secret,
+      requestOptions  = set (header "Content-Type") (["application/json"]) opts,
+      desiredColumn   = branchColumnMapping
     }
